fb46b4441ba32c7cfcdfa9241f86614d7c3069a1
Oksana <n.ksenechka.v@gmail.com>
2024-08-02 10:15:52 +0200

    update googleOAuth2

--- src/server.js
+++ src/server.js
@@ -15,37 +15,37 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 
 //дозволені домени, з яких можна робити запити
-// const allowedOrigins = [
-//   //локалхост для тестування
-//   'http://localhost:5173',
-//   'http://localhost:3000',
+const allowedOrigins = [
+  //локалхост для тестування
+  'http://localhost:5173',
+  'http://localhost:3000',
 
-//  //деплой-продакшен
-//   'https://aquatrack-taupe.vercel.app',
-//   'https://aqua-track-backend.onrender.com',
-// ];
+ //деплой-продакшен
+  'https://aquatrack-taupe.vercel.app',
+  'https://aqua-track-backend.onrender.com',
+];
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
-  // app.use(
-  //   cors({
-  //     origin: function (origin, callback) {
-  //       // Перевірте, чи є `origin` у списку дозволених
-  //       if (allowedOrigins.includes(origin) || !origin) {
-  //         // Якщо так, дозволити запит
-  //         callback(null, true);
-  //       } else {
-  //         // Інакше заборонити запит
-  //         callback(new Error('Not allowed by CORS'));
-  //       }
-  //     },
-  //     credentials: true, // Дозволити передачу файлів cookie
-  //   }),
-  // );
+  // app.use(cors());
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        // Перевірте, чи є `origin` у списку дозволених
+        if (allowedOrigins.includes(origin) || !origin) {
+          // Якщо так, дозволити запит
+          callback(null, true);
+        } else {
+          // Інакше заборонити запит
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true, // Дозволити передачу файлів cookie
+    }),
+  );
 
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
